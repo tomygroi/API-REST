86fa3a96977579d90deddcc45c36b4e50d458159
tomygroi <tommygroidirectos@gmail.com>
2026-09-03 20:24:16 -0300

    API-REST

---
+++ back/api-product/src/main/java/net/laboratorioderedes/product/entity/Product.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.laboratorioderedes.product.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="products")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+    private int quantity;
+}
---
+++ back/api-product/src/test/java/api_product/ApiProductApplicationTests.java
@@ -0,0 +1,13 @@
+package api_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
